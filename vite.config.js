b1f2230e3d92c2175b6f1225c9d33d1ec0ab503d
kasnino <16484156+kasnino@users.noreply.github.com>
2022-11-17 20:09:47 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
