6b5867f738f7bcfbc6ebe1a2527d5494c01feda1
kasnino <16484156+kasnino@users.noreply.github.com>
2022-11-17 20:03:14 -0500

    deploy productions

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
   plugins: [vue(), pluginRewriteAll()],
   server: {
     host: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html')
+      }
+    }
   } 
   
 })
